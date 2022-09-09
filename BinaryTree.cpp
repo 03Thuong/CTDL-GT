436121c56b5a00c8a859df7f2a8ff83a583699ac
03Thuong <nguyenhoangthuong825@gamil.com>
2022-09-09 11:24:35 +0700

    First commit

---
+++ BinaryTree.cpp
@@ -0,0 +1,16 @@
+#include<conio.h>
+#include<stdio.h>
+//Khai bao cau truc cay nhi phan 
+typedef struct TNode
+{
+	int key;
+	TNode *pLeft, *pRight;	
+}TNode;
+
+typedef TNode* TREE;
+
+
+int main()
+{
+	TREE myTree =NULL;
+}
